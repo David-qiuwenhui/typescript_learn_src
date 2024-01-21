1ee1b8fea6eef57f43cd75f0e054d0e33bd56582
qiuwenhui <1059713136@qq.com>
2024-01-21 16:49:20 +0800

    ✨ feat(类型基础之 条件类型和infer关键字): extends 和 infer
    新增一个条件类型和infer关键字的实例

--- index.ts
+++ index.ts
@@ -1,3 +1,26 @@
-type Status<T> = "success" | "failure" | "pending" | T;
+// 提取首尾两个
+type ExtractStartAndEnd<T extends any[]> = T extends [
+    infer Start,
+    ...any[],
+    infer End
+]
+    ? [Start, End]
+    : T;
 
-type CompleteStatus = Status<"offline">;
+// 调换首尾两个
+type SwapStartAndEnd<T extends any[]> = T extends [
+    infer Start,
+    ...infer Left,
+    infer End
+]
+    ? [End, ...Left, Start]
+    : T;
+
+// 调换开头两个
+type SwapFirstTwo<T extends any[]> = T extends [
+    infer Start1,
+    infer Start2,
+    ...infer Left
+]
+    ? [Start2, Start1, ...Left]
+    : T;
